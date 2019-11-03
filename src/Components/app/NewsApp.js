3ae0074c7ddf0c2b9868c907060fdecf2910c5c8
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-11-03 23:17:26 +0300

    feat: add MVP design pattern

--- src/Components/app/NewsApp.js
+++ src/Components/app/NewsApp.js
@@ -1,15 +1,14 @@
-import Button from '../button/Button';
-import NewsCard from '../newsCard/NewsCard';
 import './newsApp.scss';
-import NewsService from '../../api';
-import LazyImageService from '../../Services/LazyImageService';
+import NewsPage from '../../Pages/NewsPage';
+import SourcePage from '../../Pages/SourcePage';
 
-const template = '<div class="app-container"><ul class="buttons-container"><li class="back-btn">Back</li></ul><ul class="news-container"></ul></div>';
+const template = '<div class="app-container"></div>';
 
 class NewsApp {
   constructor() {
     const mainContainer = document.querySelector('.app');
     mainContainer.innerHTML = template;
+    this.container = mainContainer.querySelector('.app-container');
   }
 
   setSource(source) {
@@ -19,34 +18,13 @@ class NewsApp {
 
   async render() {
     if (!document || !document.querySelector('.app')) {
-      return;
+      return null;
     }
-
-    const buttonsContainer = document.querySelector('.app .buttons-container');
-    const newsContainer = document.querySelector('.app .news-container');
-    const backBtn = buttonsContainer.querySelector('.back-btn');
 
     if (!this.source) {
-      buttonsContainer.classList.remove('display-news');
-      newsContainer.style.display = 'none';
-      newsContainer.innerHTML = '';
-      if (!this.sources) {
-        this.sources = await NewsService.getSources();
-        this.sources.forEach((source) => {
-          buttonsContainer.append(Button(source, () => this.setSource(source.id)));
-        });
-      }
-    } else {
-      this.news = await NewsService.getNews(this.source);
-      buttonsContainer.classList.add('display-news');
-      newsContainer.style.display = 'flex';
-      backBtn.addEventListener('click', () => this.setSource(null));
-      this.news.forEach((newsInfo) => {
-        newsContainer.append(NewsCard(newsInfo));
-      });
-
-      LazyImageService.initObserver(document.querySelectorAll('img.lazy'));
+      return SourcePage(this.container, ({ id }) => this.setSource(id));
     }
+    return NewsPage(this.source, this.container, () => this.setSource(null));
   }
 }
 
